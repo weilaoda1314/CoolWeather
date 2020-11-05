ce554f8d0d99ce1b26a18e2a8b1ba9273e8e52a7
weilaoda1314 <915879483@qq.com>
2020-11-05 21:10:12 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -3,7 +3,20 @@ package com.example.coolweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.util.Xml;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TAG", "onCreate: ");
-        Log.d("TAG", "onCreate: ");
-        Log.d("TAG", "onCreate: ");
+
+        }
 
 
-    }
+
 }
